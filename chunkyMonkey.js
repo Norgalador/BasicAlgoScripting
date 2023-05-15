ba5552e3edf0613fbdf7a8cde341eaa238d23c94
dojo-JoshBurlile <josh.burlile@gmail.com>
2023-05-15 13:05:58 -0700

    added chunkyMonkey

---
+++ chunkyMonkey.js
@@ -0,0 +1,17 @@
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+    // let newArr = [];
+    // let spliceArr = arr.splice(0, size);
+    // console.log(spliceArr);
+    // let newArr = [spliceArr, arr];
+    // console.log(newArr);
+    // return newArr;
+    let newArr = [];
+    for (let i = 0; i < arr.length; i += size){
+      newArr.push(arr.slice(i, i + size));
+    }
+    return newArr;
+  }
+  
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);
